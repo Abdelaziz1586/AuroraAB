2e1781494b617d0b76a24f34cdaeb9efabdd75cd
NoTarget <notargetofficial@gmail.com>
2022-04-04 04:13:49 +0200

    Decreased the plugin's size.

--- Aurora-bungeecord/src/main/java/aurora/aurora/AuroraLoggerBungee.java
+++ Aurora-bungeecord/src/main/java/aurora/aurora/AuroraLoggerBungee.java
@@ -2,35 +2,26 @@ package aurora.aurora;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.log.ConciseFormatter;
-import org.apache.logging.log4j.core.Filter;
-import org.apache.logging.log4j.core.filter.AbstractFilter;
+import java.util.logging.Logger;
 
 import static aurora.aurora.AuroraBungeeCord.ConsoleFilter;
 
-public class AuroraLoggerBungee extends AbstractFilter implements Filter {
+public class AuroraLoggerBungee {
 
     public void registerFilter() {
         if (ProxyServer.getInstance().getVersion().contains("Waterfall")) {
             new AuroraLoggerBungeeLog4J().registerFilter();
-            ProxyServer.getInstance().getLogger().info("§aEnabled Log4J Filters");
         } else {
-            ProxyServer.getInstance().getLogger().info("§eEnabling Default Filters...");
-            java.util.logging.Logger logger = ProxyServer.getInstance().getLogger();
+            Logger logger = ProxyServer.getInstance().getLogger();
             logger.setFilter(record -> {
-                if(ConsoleFilter) {
-                    String msg = (new ConciseFormatter(false)).formatMessage(record).trim();
-                    return logMessage(msg);
-                }
-                return false;
+                String msg = (new ConciseFormatter(false)).formatMessage(record).trim();
+                return logMessage(msg);
             });
-            ProxyServer.getInstance().getLogger().info("§aEnabled Default Filters");
         }
     }
 
     public static boolean logMessage(String message) {
-        if (message.contains("lost connection:") && message.contains("Aurora")) {
-            return false;
-        }
+        if(!ConsoleFilter) return true;
         return !message.contains(" <-> InitialHandler");
     }
 
